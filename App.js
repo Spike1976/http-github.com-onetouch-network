bc708b23cbc9a37bea93be0ef5b6be49ced0589d
Spike1976 <spikestokes@gmail.com>
2025-01-26 11:51:16 -0600

    Create App.js
    Main App
    
    Path: frontend/src/App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SDRVote from './components/SDRVote';
+import SDRStatus from './components/SDRStatus';
+
+const App = () => (
+  <div>
+    <h1>OneTouch Network</h1>
+    <SDRVote />
+    <SDRStatus />
+  </div>
+);
+
+export default App;
